becfa1a42666573a1e7f7a6687e3772dc600f769
saishanker41 <69297472+saishanker41@users.noreply.github.com>
2020-08-06 15:39:00 +0530

    Create fab.py

---
+++ fab.py
@@ -0,0 +1,15 @@
+def fab(num):
+    a = 0
+    b = 0
+    if num == 0:
+       print("Invalid number"):
+    elif num == 1:
+       return a
+    else:
+       print(a)
+       print(b)
+       c = a + b
+       a = b
+       b = c
+       print(c)
+print(fab(12))    
